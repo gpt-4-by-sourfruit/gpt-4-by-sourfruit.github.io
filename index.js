94156153ec3566df13775f18ac60a194ae2547bd
Sourfruit <65507526+hack-everything@users.noreply.github.com>
2020-12-14 12:18:46 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-console.log('Oh hi there');
-
-function ohNo() {
-    alert("Oh no! Seems like this plan is not available for purchase yet! Please try again later.");
- }
-
-document.cookie('WelcomeToGpt-4.co')
